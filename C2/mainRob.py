4e9f06bbcf656d6f9b68e91860deeffd2474ac8b
Manuel <gomesnelito@gmail.com>
2021-11-01 17:04:36 +0000

    New functions not functional

--- C2/mainRob.py
+++ C2/mainRob.py
@@ -5,12 +5,16 @@ from croblink import *
 from math import *
 import xml.etree.ElementTree as ET
 
+
+
 CELLROWS=7
 CELLCOLS=14
 
 class MyRob(CRobLinkAngs):
     def __init__(self, rob_name, rob_id, angles, host):
         CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
+        self.posList = []
+        self.errList = []
 
     # In this map the center of cell (i,j), (i in 0..6, j in 0..13) is mapped to labMap[i*2][j*2].
     # to know if there is a wall on top of cell(i,j) (i in 0..5), check if the value of labMap[i*2+1][j*2] is space or not
@@ -66,7 +70,6 @@ class MyRob(CRobLinkAngs):
 
     def wander(self):
         maze=Lab()
-
         center_id = 0
         left_id = 1
         right_id = 2
@@ -78,9 +81,32 @@ class MyRob(CRobLinkAngs):
         right_sensor = self.measures.irSensor[right_id]
         back_sensor = self.measures.irSensor[back_id]
         ground_sensor = self.measures.ground
-        compass=self.measures.compass
-        x=self.measures.x
-        y=self.measures.y
+        compass = self.measures.compass
+        x = self.measures.x
+        y = self.measures.y
+
+        self.moveFront(2, 0.2, 3, 0.001)
+
+        # if counter >= 2:
+        #     linVel = self.getLinVel(x, 1)
+        #     # self.errorCalculator(lvSet, linVel)
+        #     # lin = self.PIDControler(linVel, 0, 0, 0)
+        #     lin = 0.15
+        #     rot = 0
+        #     print(linVel)
+        # else:
+        #     lin = 0.15
+        #     rot = 0
+        #     self.errorCalculator(lvSet, lin)
+        #
+        # self.converter(lin, rot)
+        # counter += 1
+        # if center_sensor < 5.0 and center_sensor > 0.2:
+        #     lin = 0.15 / center_sensor
+        # elif center_sensor < 0.2:
+        #     lin = 0.15
+        # else:
+        #     lin = 0
 
         
 
@@ -88,10 +114,72 @@ class MyRob(CRobLinkAngs):
         f=open('mapping.txt','w')
 
         f.close()
+
+
+    def getLinVel(self, list, timeInt):
+        currPos = self.posList[-1]
+        prevPos = self.posList[-2]
+
+        linVel = (currPos - prevPos) / timeInt
+
+        return linVel
+
+    def getAngVel(self, currAng, timeInt):
+        global prevAng
+
+        if prevAng is None:
+            prevAng = 0
+
+        angVel = currAng - prevAng / timeInt
+        prevAng = currAng
+
+        return angVel
+
+    def errorCalculator(self, setpoint, current):
+        error = setpoint - current
+        self.errList.append(error)
+        return
+
+    def PIDControler(self,  prev, K0, K1, K2):
+        error_3 = self.errList[-3]
+        error_2 = self.errList[-2]
+        error_1 = self.errList[-1]
+
+        vel = prev + K0 * error_1 + K1 * error_2 + K2 * error_3
+        print('sent vel: ' + str(vel))
+
+        return vel
+
+    def moveFront(self, length, Kp, Kd, Ki):
+        xin = self.measures.x
+        xobj = xin + length
+        lin = 0.15
+        integral = 0
+        print('test')
+        while length > 0:
+            x = self.measures.x
+            err = xobj - x
+            print(err)
+            diff = err / lin
+            integral += err
+            lin = Kp * err + Kd * diff + Ki * integral
+            print(lin)
+            length -= err
+            print('length: ' + str(length))
+            self.converter(lin, 0)
+
+
+
+
+    def converter(self, lin, rot):
+        left_motor = lin - rot / 2
+        right_motor = lin + rot / 2
+        self.driveMotors(left_motor, right_motor)
+
 class Lab():
     def __init__(self):
-        self.colunms=[' ']*(CELLCOLS*4)-1
-        self.rows=[' ']*(CELLROWS*4)-1
+        self.colunms=[' ']*(CELLCOLS*4-1)
+        self.rows=[' ']*(CELLROWS*4-1)
     
 class Map():
     def __init__(self, filename):
